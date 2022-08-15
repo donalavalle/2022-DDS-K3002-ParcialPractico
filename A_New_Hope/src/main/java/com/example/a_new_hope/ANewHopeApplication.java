e3f7afca2b8e0fea191a5ee5dfd1ae10a027c4fb
Lautaro-L <lautilazo@gmail.com>
2022-08-15 20:20:48 -0300

    agrego carpetas y dsp los archivos

---
+++ A_New_Hope/src/main/java/com/example/a_new_hope/ANewHopeApplication.java
@@ -0,0 +1,13 @@
+package com.example.a_new_hope;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ANewHopeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ANewHopeApplication.class, args);
+    }
+
+}
---
+++ A_New_Hope/src/test/java/com/example/a_new_hope/ANewHopeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.a_new_hope;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ANewHopeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
